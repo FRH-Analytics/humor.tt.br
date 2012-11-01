429751bb6fc95e2f00e4bc3f6d57e72bdc65fb91
nigini <nigini@gmail.com>
2012-11-01 09:21:00 -0300

    Atualizando código que conta tweets e tts coletados e armazenados no banco.

--- coletor/count_tweets.py
+++ coletor/count_tweets.py
@@ -1,7 +1,13 @@
 from pymongo import Connection
+import conf
+
+mongo_dbname=conf.MONGO_DBNAME
+
 con = Connection()
-db = con['Analytics_Tweets']
+db = con[mongo_dbname]
+
 trends_db = db['trends']
 tweets_db = db['tweets'] 
+
 print 'trends= ', trends_db.count()
 print 'tweets= ', tweets_db.count()
