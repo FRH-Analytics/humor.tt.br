2b16649ee18d2b1690aeae81fe8cd03914cc5420
Pedro Limeira <pedro@atena.(none)>
2012-11-15 13:04:55 -0300

    detalhes corrigidos

--- scripts/agrupapordia.py
+++ scripts/agrupapordia.py
@@ -2,6 +2,10 @@ import sys
 
 #TODO fazer a saida ser ordenada pelo tempo
 
+if len(sys.argv) != 3:
+  print "Usage: python agrupapordia.py <arquivo entrada> <arquivo saida>"
+  sys.exit()
+
 dicionario_humor_dia  = {}
 dicionario_humor_hora = {}
 
@@ -12,21 +16,18 @@ def extraiDiaMes(timestamp):
   res = timestamp.split(' ')
   return "%s/%s"%(res[1],res[2])
   
-if len(sys.argv) != 3:
-  print "Usage: python agrupapordia.py <arquivo entrada> <arquivo saida>"
-else: 
-  for linha in linhas:
-    linha_campos = linha.split('\t')
-    dia_mes = extraiDiaMes(linha_campos[0])
-    if dicionario_humor_dia.has_key(dia_mes):
-      dicionario_humor_dia[dia_mes] = dicionario_humor_dia[dia_mes] + float(linha_campos[len(linha_campos) -1])
-    else:
-      dicionario_humor_dia[dia_mes] = float(linha_campos[2])
+for linha in linhas:
+  linha_campos = linha.split('\t')
+  dia_mes = extraiDiaMes(linha_campos[0])
+  if dicionario_humor_dia.has_key(dia_mes):
+    dicionario_humor_dia[dia_mes] = dicionario_humor_dia[dia_mes] + float(linha_campos[len(linha_campos) -1])
+  else:
+    dicionario_humor_dia[dia_mes] = float(linha_campos[2])
 
 
-  arquivo_out = open(sys.argv[2],'a')
+arquivo_out = open(sys.argv[2],'a')
 
-  for chave in dicionario_humor_dia.keys():
-    arquivo_out.write("%s,%s\n"%(chave, dicionario_humor_dia[chave]))
+for chave in dicionario_humor_dia.keys():
+  arquivo_out.write("%s,%s\n"%(chave, dicionario_humor_dia[chave]))
 
-  arquivo_out.close()
+arquivo_out.close()
