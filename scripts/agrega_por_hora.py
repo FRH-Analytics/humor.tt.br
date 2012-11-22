0ff94022155e4067b8ef602c0768dd60cb18bfff
Pedro Limeira <pedro@atena.(none)>
2012-11-22 02:19:19 -0300

    agregacao por hora com ordenacao.

--- scripts/agrega_por_hora.py
+++ scripts/agrega_por_hora.py
@@ -12,16 +12,17 @@ def calculaMedia(lista):
       continue
   return sum(lista_aux)/len(lista_aux)
 
-def capturadia(linha):
+def capturahora(linha):
   linha_campos = linha.split('\t')
   timestamp_split = linha_campos[0].split(' ')
+  hora= timestamp_split[4].split(':')[0]
   ano = timestamp_split[3]
   mes = meses[timestamp_split[2]]
   dia = timestamp_split[1]
-  return "%s/%s/%s"%(dia,mes,ano)
+  return "%s/%s/%s/%s"%(dia,mes,ano,hora)
 
 if len(sys.argv) != 3:
-  print "Uso: python agrega_por_dia.py <arquivo com tweets classificados> <arquivo de saida>"
+  print "Uso: python agrega_por_hora.py <arquivo com tweets classificados> <arquivo de saida>"
   sys.exit()
 
 POLARIDADE = 2
@@ -30,18 +31,18 @@ arquivo_de_tweets = open(sys.argv[1], 'r')
 
 for linha in arquivo_de_tweets.readlines():
   try:  
-    dia = capturadia(linha)
+    hora = capturahora(linha)
     linha_sep = linha.split('\t')
 
     if len(linha_sep) != 5:
       continue
 
     else:
-      if dic_dia_polaridades.has_key(dia):
-        dic_dia_polaridades[dia].append(linha_sep[POLARIDADE])
+      if dic_dia_polaridades.has_key(hora):
+        dic_dia_polaridades[hora].append(linha_sep[POLARIDADE])
       else:
-        dic_dia_polaridades[dia] = []
-        dic_dia_polaridades[dia].append(linha_sep[POLARIDADE])
+        dic_dia_polaridades[hora] = []
+        dic_dia_polaridades[hora].append(linha_sep[POLARIDADE])
   except (KeyError, IndexError):
     continue
 
